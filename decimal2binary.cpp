e14258f18edd04e8060ff646a403992eb6ceffa5
jaiveer077 <jaiveersagoo@gmail.com>
2025-12-25 14:39:41 +0530

    first

--- decimal2binary.cpp
+++ decimal2binary.cpp
@@ -4,5 +4,14 @@ using namespace std;
 int main(){
     ll n;
     cout<<"Enter a num: "; cin>>n;
-    
+    string res="";
+    ll temp=n;
+    while(temp!=0){
+        if(temp%2==1) res+="1";
+        else if(temp%2==0) res+="0";
+        temp/=2;
+    }
+    reverse(res.begin(),res.end());
+    cout<<"Binary string: "<<res<<endl;
+    return 0;
 }
