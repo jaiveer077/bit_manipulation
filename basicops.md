dac647504545de99ea85a793747282055da84db0
jaiveer077 <jaiveersagoo@gmail.com>
2025-12-25 18:43:28 +0530

    final changes

---
+++ basicops.md
@@ -0,0 +1,4 @@
+| will be used to set 
+^ will be used to toggle 
+& will be used to check 
+&~ will be used to clear 
--- powerset.cpp
+++ powerset.cpp
@@ -1,34 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool checkSetBit(int n,int i){
-    if(n&(1<<i)){
-        return true;
-    }
-    return false;
-}
 int main(){
+    vector<vector<int>> answer;
     vector<int> nums={1,2,3};
-    vector<vector<int>> result;
     int n=nums.size();
-    int a=nums[0],b=nums[1],c=nums[2];
-    for(int i=0;i<1<<n;i++){ // 1<<n = 2^n
+    for(int i=0;i<(1<<n);i++){
         vector<int> temp;
-        if(checkSetBit(i,0)){
-            temp.push_back(nums[0]);
-        } 
-        if(checkSetBit(i,1)){
-            temp.push_back(nums[1]);
-        } 
-        if(checkSetBit(i,2)){
-            temp.push_back(nums[2]);
+        for(int j=0;j<n;j++){
+            if(i&(1<<j)){
+                temp.push_back(nums[j]);
+            }
         }
-        result.push_back(temp);
+        answer.push_back(temp);
     }
-    sort(result.begin(),result.end());
-    for(auto &row:result){
+    for(auto &row:answer){
         for(auto c:row){
-            cout<<"["<<c<<"]";
-        }cout<<" ";
+            cout<<c<<" ";
+        }cout<<endl;
     }
     return 0;
 }
